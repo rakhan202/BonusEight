febdf97bf3f77d4137b0a0cea948ddf69fcedfa5
Rizwan Khan <khan.a.rizwan@gmail.com>
2017-07-11 09:32:12 -0400

    second commit

--- src/Movie.java
+++ src/Movie.java
@@ -2,48 +2,41 @@
  * Created by Rizwan on 7/7/2017.
  */
 
-public class Movie {
 
-    public class Movie implements Comparable
-    {
+    public class Movie implements Comparable {
         private String title;
         private String genre;
 
 
-        public Movie(String title, String genre)
-        {
+        public Movie(String title, String genre) {
             this.title = title;
             this.genre = genre;
         }
 
-        public String getTitle()
-        {
+        public String getTitle() {
             return title;
         }
 
-        public void setTitle(String title)
-        {
+        public void setTitle(String title) {
             this.title = title;
         }
 
-        public String getGenre()
-        {
+        public String getGenre() {
             return genre;
         }
 
-        public void setGenre(String genre)
-        {
+        public void setGenre(String genre) {
             this.genre = genre;
         }
 
+
         @Override
-        public int compareTo(Object o)
-        {
+        public int compareTo(Object o) {
             Movie m = (Movie) o;
             int titleCompare = this.getTitle().compareTo(m.getTitle());
             if (titleCompare != 0)
                 return titleCompare;
             else
-                return(this.getTitle().compareToIgnoreCase(m.getTitle()));
+                return (this.getTitle().compareToIgnoreCase(m.getTitle()));
         }
     }
